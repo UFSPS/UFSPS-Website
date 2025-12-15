bba7da54cb2d2619a3253f7a2eab1d22137c4dd0
tiffh <tiffhuang321@gmail.com>
2025-12-15 00:25:08 -0500

    feat: front content

--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -6,6 +6,37 @@ const Frontpage = () => {
         <>
         <div className='page-container'>
             <Navbar />
+               
+      {/* --- Main Hero Section --- */}
+      <main className="front-container">
+        
+        {/* Left Column: Text Content */}
+        <div className="front-content">
+          <h1>
+            Signal<br />
+            Processing<br />
+            Society
+          </h1>
+          
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus purus et nulla facilisis, eu fringilla leo sagittis. Nam quis.
+          </p>
+
+          <button className="front-button">
+            Join Discord
+          </button>
+        </div>
+
+        {/* Right Column: Illustration */}
+        <div className="front-graphic">
+          <svg viewBox="0 0 500 500" className="wireframe-svg">
+            {/* Three overlapping circles */}
+            <circle cx="240" cy="160" r="180" />
+            <circle cx="200" cy="200" r="180" />
+            <circle cx="160" cy="240" r="180" />
+          </svg>
+        </div>
+      </main>
         </div>
         </>
 
