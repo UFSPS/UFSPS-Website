ee1295762e046c128217850a59cece33f2c577e5
tiffh <tiffhuang321@gmail.com>
2025-12-15 00:31:01 -0500

    feat: scroll indicator

--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -1,45 +1,45 @@
-import './styles/Frontpage.css'
+import { ChevronsDown } from 'lucide-react';
+import './styles/Frontpage.css';
 import Navbar from '../components/Navbar';
 
 const Frontpage = () => {
     return (
         <>
-        <div className='page-container'>
-            <Navbar />
-               
-      {/* --- Main Hero Section --- */}
-      <main className="front-container">
-        
-        {/* Left Column: Text Content */}
-        <div className="front-content">
-          <h1>
-            Signal<br />
-            Processing<br />
-            Society
-          </h1>
-          
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus purus et nulla facilisis, eu fringilla leo sagittis. Nam quis.
-          </p>
+            <div className='page-container'>
+                <Navbar />
+                <main className="front-container">
+                    <div className="front-content">
+                    <h1>
+                        Signal<br />
+                        Processing<br />
+                        Society
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus purus et nulla facilisis, eu fringilla leo sagittis. Nam quis.
+                    </p>
 
-          <button className="front-button">
-            Join Discord
-          </button>
-        </div>
+                    {/* Discord button */}
+                    <button className="front-button">
+                        Join Discord
+                    </button>
+                    </div>
 
-        {/* Right Column: Illustration */}
-        <div className="front-graphic">
-          <svg viewBox="0 0 500 500" className="wireframe-svg">
-            {/* Three overlapping circles */}
-            <circle cx="240" cy="160" r="180" />
-            <circle cx="200" cy="200" r="180" />
-            <circle cx="160" cy="240" r="180" />
-          </svg>
-        </div>
-      </main>
-        </div>
+                    {/* Page graphics */}
+                    <div className="front-graphic">
+                    <svg viewBox="0 0 500 500" className="wireframe-svg">
+                        <circle cx="240" cy="160" r="180" />
+                        <circle cx="200" cy="200" r="180" />
+                        <circle cx="160" cy="240" r="180" />
+                    </svg>
+                    </div>
+                </main>
+            
+                <div className="front-scroll">
+                    <ChevronsDown size={32} color="#9ca3af" strokeWidth={1} />
+                    <span>scroll down for calendar</span>
+                </div>
+            </div>
         </>
-
     );
 };
 
