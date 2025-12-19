76cb2cfedc71804876530ce9290329d68ed3e1e5
tiffh <tiffhuang321@gmail.com>
2025-12-19 01:18:53 -0500

    style: add logo
    todo: change navbar styling and margins

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,30 @@
 import './styles/Navbar.css';
 import { Search } from 'lucide-react';
+import Logo from '../assets/SPS_Logo.png';
 
 const Navbar = () => {
     return (
         <header className="navbar">
         <div className="logo-section">
-          <div className="logo-circle"></div>
-          <span className="logo-text">Logo</span>
+          <img src={Logo} className="logo"/>
+          <div className="logo-div"></div>
+          <p className="logo-text">
+            <span style={{ fontWeight: 700 }}>University of Florida</span>
+            <br/>
+            IEEE Student Branch
+          </p>
         </div>
 
         <nav className="nav-pill">
           <a href="#home">Home</a>
           <a href="#about">About</a>
-          <a href="#journals">The Journals</a>
+          <a href="#journals">Journals</a>
           <a href="#workshops">Workshops</a>
         </nav>
 
         <div className="search-icon">
           <button>
-            <Search size={20} color="#4b5563" />
+            <Search size={22} color="#4b5563" />
           </button>
         </div>
       </header>
