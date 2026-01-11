f1640964ea6019ce0794f4224982ea3ee7cf8b6c
tiffh <tiffhuang321@gmail.com>
2026-01-11 01:26:19 -0500

    small front page changes

--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -2,14 +2,13 @@ import { ChevronsDown } from 'lucide-react';
 import './styles/Frontpage.css';
 import Navbar from '../components/Navbar';
 import PageContainer from '../components/PageContainer';
+import Calendar from '../components/Calendar';
 import Image from '../assets/background.png';
 
 const Frontpage = () => {
     return (
         <>
             <PageContainer 
-                // 2. Add inline style for multiple backgrounds
-                // The Gradient comes FIRST (Top Layer), the URL comes SECOND (Bottom Layer)
                 style={{
                     backgroundImage: `
                         url("${Image}")
@@ -29,7 +28,7 @@ const Frontpage = () => {
                     </h1>
                     <p>
                         For engineers, mathematicians, and scientists. We receive, analyze, process,
-                        and transmit signals. Everything in life can be represented by a signal, explore
+                        and transmit signals. Everything in life can be represented by a signal. Explore
                         reality through the digital interface.
                     </p>
                     <p>
@@ -45,11 +44,14 @@ const Frontpage = () => {
                     </div>
                 </main>
             
+                {/* Note: Optional in case we want to implement a calendar
                 <div className="front-scroll">
                     <ChevronsDown size={32} color="#9ca3af" strokeWidth={1} />
                     <span>scroll down for calendar</span>
                 </div>
+                */}
             </PageContainer>
+            <Calendar />
         </>
     );
 };
