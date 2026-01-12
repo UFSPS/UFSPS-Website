94485244ffb3aa620d2434aa3ff4e71fd15c04d6
tiffh <tiffhuang321@gmail.com>
2026-01-11 23:37:21 -0500

    icon package + social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import './styles/Footer.css';
 import Logo from '../assets/SPS_Logo_Black.png';
-
+import { SiInstagram, SiDiscord, SiLinkedin, SiLinktree, SiX } from 'react-icons/si';
 const Footer = () => {
     return (
         <div className="footer-container">
@@ -9,7 +9,11 @@ const Footer = () => {
                 <img className="footer-graphic" src={Logo} alt="black sps logo" />
                 <div className="footer-link-container">
                     <div className="social-links">
-
+                        <SiInstagram />
+                        <SiDiscord />
+                        <SiLinkedin />
+                        <SiX />
+                        <SiLinktree />
                     </div>
                     <div className="email-link">
                         
