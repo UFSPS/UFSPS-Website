2abbcc67fbb81236a7a65f3b085b0157f632b712
tiffh <tiffhuang321@gmail.com>
2026-01-12 00:13:18 -0500

    + footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import './styles/Footer.css';
 import Logo from '../assets/SPS_Logo_Black.png';
 import { SiInstagram, SiDiscord, SiLinkedin, SiLinktree, SiX } from 'react-icons/si';
+import { FiMail } from 'react-icons/fi';
+
 const Footer = () => {
     return (
         <div className="footer-container">
@@ -15,11 +17,17 @@ const Footer = () => {
                         <SiX />
                         <SiLinktree />
                     </div>
-                    <div className="email-link">
-                        
-                    </div>
-                    <p className="location-text">
 
+                    <a href="mailto:ieee.sps.uf@gmail.com">
+                        <div className="email-link">
+                            <FiMail />
+                        <p>ieee.sps.uf@gmail.com</p>
+                        </div>
+                    </a>
+                    <p className="location-text">
+                        Artificial Intelligence and Informatics Research Institute <br/>
+                        432 Newell Drive, CISE Bldg E251 <br/>
+                        Gainesville, Florida 32611-5585
                     </p>
                 </div>
             
