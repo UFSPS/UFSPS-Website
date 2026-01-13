8fb2a965914abf9a6c67dd47cc2d83615671e084
tiffh <tiffhuang321@gmail.com>
2026-01-13 13:42:01 -0500

    init about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar.js';
+
+const About = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="section-container">
+                
+            </div>
+        </>
+    );
+};
+
+export default About;
