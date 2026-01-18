2ca8fa09e944184c463ec8b9d822291f4084b85c
tiffh <tiffhuang321@gmail.com>
2026-01-17 19:31:56 -0500

    minor root variable changes
    add an extra color value

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'; // Make sure to import the CSS file
 import Frontpage from './pages/Frontpage';
 import About from './pages/About';
+import Research from './pages/Research';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Frontpage />} />
           <Route path="/about" element={<About />} />
+          <Route path="/research" element={<Research />} />
         </Routes>
       </BrowserRouter>
   );
