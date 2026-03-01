513b4030c6ec8cfb8bfd6a290a8f9691d25a72e9
tiffh <tiffhuang321@gmail.com>
2026-03-01 18:51:49 -0500

    refactor: asset file directory

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import './styles/Navbar.css';
-import Logo from '../assets/SPS_Logo.png';
+import Logo from '../assets/logo/SPS_Logo.png';
 import { useState, useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
