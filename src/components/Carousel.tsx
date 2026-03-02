677981b8a8e154a981ac4d900c9787884ea2519a
tiffh <tiffhuang321@gmail.com>
2026-03-01 22:11:35 -0500

    carousel adjustments

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,6 +6,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 interface ImageList {
     images: string[];
 }
+
 const Carousel = ({ images }: ImageList) => {
     const [currentIndex, setCurrentIndex] = useState(0);
     
@@ -24,42 +25,42 @@ const Carousel = ({ images }: ImageList) => {
     };
 
     return (
-    <div className="carousel-images">
-        <img
-          key={currentIndex}
-          src={images[currentIndex]}
-        /><div className="slide_direction">
-        <div className="left" onClick={handlePrevious}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="20"
-            viewBox="0 96 960 960"
-            width="20"
-          >
-            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-          </svg>
-        </div>
-        <div className="right" onClick={handleNext}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="20"
-            viewBox="0 96 960 960"
-            width="20"
-          >
-            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-          </svg>
+    <>
+      <div className="carousel-images">
+        <img key={currentIndex} src={images[currentIndex]}/>
+        <div className="slide_direction">
+          <div className="left" onClick={handlePrevious}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              viewBox="0 96 960 960"
+              width="20"
+            >
+              <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+            </svg>
+          </div>
+          <div className="right" onClick={handleNext}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              viewBox="0 96 960 960"
+              width="20"
+            >
+              <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+            </svg>
+          </div>
         </div>
       </div>
-      <div className="carousel-indicator">
-        {images.map((_, index) => (
-          <div
-            key={index}
-            className={`dot ${currentIndex === index ? "active-image" : ""}`}
-            onClick={() => handleDotClick(index)}
-          ></div>
-        ))}
-      </div>
-    </div>
+      {/* <div className="carousel-indicator">
+          {images.map((_, index) => (
+            <div
+              key={index}
+              className={`dot ${currentIndex === index ? "active-image" : ""}`}
+              onClick={() => handleDotClick(index)}
+            />
+          ))}
+      </div> */}
+    </>
   );
 };
 export default Carousel;
