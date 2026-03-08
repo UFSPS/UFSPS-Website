9959f8830ec20ec43f49a3df3e559885f7206b8b
tiffh <tiffhuang321@gmail.com>
2026-03-08 11:45:25 -0400

    update: eboard.ts object keys quote removal
    i think it looks cleaner

--- src/data/eboard.ts
+++ src/data/eboard.ts
@@ -7,23 +7,23 @@ interface Profile {
 export const eboardProfiles: Record<string, Profile> = 
 {
     "President": {
-        "name": "Raul Valle",
-        "githubLink": "https://github.com/Jibby2k1",
-        "linkedinLink": "https://www.linkedin.com/in/raul-valle1/"
+        name: "Raul Valle",
+        githubLink: "https://github.com/Jibby2k1",
+        linkedinLink: "https://www.linkedin.com/in/raul-valle1/"
     },
     "Vice President": {
-        "name": "Matheus Kunzler Maldaner",
-        "githubLink": "https://github.com/matheusmaldaner",
-        "linkedinLink": "https://www.linkedin.com/in/matheusmaldaner/"
+        name: "Matheus Kunzler Maldaner",
+        githubLink: "https://github.com/matheusmaldaner",
+        linkedinLink: "https://www.linkedin.com/in/matheusmaldaner/"
     },
     "Chief of Operations": {
-        "name": "Tiffany Huang",
-        "githubLink": "https://github.com/narcistiq",
-        "linkedinLink": "https://www.linkedin.com/in/tiffanyhuang1010/"
+        name: "Tiffany Huang",
+        githubLink: "https://github.com/narcistiq",
+        linkedinLink: "https://www.linkedin.com/in/tiffanyhuang1010/"
     },
     "Chief of Education": {
-        "name": "Awwab Azam",
-        "githubLink": "https://github.com/kaddu341",
-        "linkedinLink": "https://www.linkedin.com/in/awwab-ali-azam-101a4b257"
+        name: "Awwab Azam",
+        githubLink: "https://github.com/kaddu341",
+        linkedinLink: "https://www.linkedin.com/in/awwab-ali-azam-101a4b257"
     }
 }
