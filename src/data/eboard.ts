5a90f0135f1890078a48c5ea0beac660c239c3b7
tiffh <tiffhuang321@gmail.com>
2026-04-23 22:54:44 -0400

    refactor: json to object literal

--- src/data/eboard.ts
+++ src/data/eboard.ts
@@ -7,21 +7,21 @@ interface Profile {
 export const eboardProfiles: Record<string, Profile> = 
 {
     "President": {
-        "name": "Raul Valle",
-        "githubLink": "https://github.com/Jibby2k1",
-        "linkedinLink": "https://www.linkedin.com/in/raul-valle1/"
+        name: "Raul Valle",
+        githubLink: "https://github.com/Jibby2k1",
+        linkedinLink: "https://www.linkedin.com/in/raul-valle1/"
     },
     "Vice President": {
-        "name": "Matheus Kunzler Maldaner"
+        name: "Matheus Kunzler Maldaner"
     },
     "Chief of Operations": {
-        "name": "Tiffany Huang",
-        "githubLink": "https://github.com/narcistiq",
-        "linkedinLink": "https://www.linkedin.com/in/tiffanyhuang1010/"
+        name: "Tiffany Huang",
+        githubLink: "https://github.com/narcistiq",
+        linkedinLink: "https://www.linkedin.com/in/tiffanyhuang1010/"
     },
     "Chief of Education": {
-        "name": "Awwab Azam",
-        "githubLink": "https://github.com/narcistiq",
-        "linkedinLink": "https://www.linkedin.com/in/tiffanyhuang1010/"
+        name: "Awwab Azam",
+        githubLink: "https://github.com/narcistiq",
+        linkedinLink: "https://www.linkedin.com/in/tiffanyhuang1010/"
     }
 }
